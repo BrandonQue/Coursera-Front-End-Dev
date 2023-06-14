8bb41fc8bb34e5d71272cf972553f630756bcc13
Brandon Quezada <brandon_que@hotmail.com>
2023-06-14 15:44:55 -0700

    add "Passing Props" lab files

---
+++ React Basics/Passing Props/passing-props/src/App.js
@@ -0,0 +1,14 @@
+import Heading from './Heading';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="Brandon"/>
+      <Heading firstName="Annery"/>
+      <Heading firstName="Jayson"/>
+      <Heading firstName="David"/>
+    </div>
+  );
+}
+
+export default App;
